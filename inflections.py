8af5ab066799eae21dadae9ecfe07149fabd5edd
kohchingay <kohchingay@gmail.com>
2025-08-06 17:42:04 +0800

    Update inflections.py

--- inflections.py
+++ inflections.py
@@ -5,8 +5,8 @@ import pandas as pd
 st.markdown("""
     <style>
         .block-container {
-            padding-left: 3rem;
-            padding-right: 3rem;
+            padding-left: 10rem;
+            padding-right: 10rem;
             padding-top: 1rem;
             padding-bottom: 1rem;
         }
