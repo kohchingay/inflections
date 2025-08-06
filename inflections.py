1a7825e5adfeefe7d8bbab83d88c2b4ebaf9a6e6
kohchingay <kohchingay@gmail.com>
2025-08-06 17:30:25 +0800

    Update inflections.py

--- inflections.py
+++ inflections.py
@@ -50,39 +50,39 @@ def show_three(title1, data1, title2, data2, title3, data3):
 # Present Indicative
 present_active = [["1st", "λύω", "λύομεν"], ["2nd", "λύεις", "λύετε"], ["3rd", "λύει", "λύουσι(ν)"]]
 present_mp = [["1st", "λύομαι", "λυόμεθα"], ["2nd", "λύῃ", "λύεσθε"], ["3rd", "λύεται", "λύονται"]]
-show_two("Pres. Ind. Act.", present_active, "Pres. Ind. Mid./Pass.", present_mp)
+show_two("Pres. Act. Ind.", present_active, "Pres. Mid./Pass. Ind.", present_mp)
 
 # Imperfect Indicative
 imperfect_active = [["1st", "ἔλυον", "ἐλύομεν"], ["2nd", "ἔλυες", "ἐλύετε"], ["3rd", "ἔλυε(ν)", "ἔλυον"]]
 imperfect_mp = [["1st", "ἐλυόμην", "ἐλυόμεθα"], ["2nd", "ἐλύου", "ἐλύεσθε"], ["3rd", "ἐλύετο", "ἐλύοντο"]]
-show_two("Imp. Ind. Act.", imperfect_active, "Imp. Ind. Mid./Pass.", imperfect_mp)
+show_two("Imp. Act. Ind.", imperfect_active, "Imp. Mid./Pass. Ind.", imperfect_mp)
 
 # Future Indicative
 future_active = [["1st", "λύσω", "λύσομεν"], ["2nd", "λύσεις", "λύσετε"], ["3rd", "λύσει", "λύσουσι(ν)"]]
 future_middle = [["1st", "λύσομαι", "λυσόμεθα"], ["2nd", "λύσῃ", "λύσεσθε"], ["3rd", "λύσεται", "λύσονται"]]
 future_passive = [["1st", "λυθήσομαι", "λυθησόμεθα"], ["2nd", "λυθήσῃ", "λυθήσεσθε"], ["3rd", "λυθήσεται", "λυθήσονται"]]
-show_three("Fut. Ind. Act.", future_active, "Fut. Ind. Mid.", future_middle, "Fut. Ind. Pass.", future_passive)
+show_three("Fut. Act. Ind.", future_active, "Fut. Mid. Ind.", future_middle, "Fut. Pass. Ind.", future_passive)
 
 # 1st Aorist Indicative
 aorist1_active = [["1st", "ἔλυσα", "ἐλύσαμεν"], ["2nd", "ἔλυσας", "ἐλύσατε"], ["3rd", "ἔλυσε(ν)", "ἔλυσαν"]]
 aorist1_middle = [["1st", "ἐλυσάμην", "ἐλυσάμεθα"], ["2nd", "ἐλύσω", "ἐλύσασθε"], ["3rd", "ἐλύσατο", "ἐλύσαντο"]]
 aorist1_passive = [["1st", "ἐλύθην", "ἐλύθημεν"], ["2nd", "ἐλύθης", "ἐλύθητε"], ["3rd", "ἐλύθη", "ἐλύθησαν"]]
-show_three("1st Aorist Indicative Active", aorist1_active, "1st Aorist Indicative Middle", aorist1_middle, "1st Aorist Indicative Passive", aorist1_passive)
+show_three("1st Aor. Act. Ind.", aorist1_active, "1st Aor. Mid. Ind.", aorist1_middle, "1st Aor. Pass. Ind.", aorist1_passive)
 
 # 2nd Aorist Indicative
 aorist2_active = [["1st", "ἔλαβον", "ἐλάβομεν"], ["2nd", "ἔλαβες", "ἐλάβετε"], ["3rd", "ἔλαβε(ν)", "ἔλαβον"]]
 aorist2_middle = [["1st", "ἐλαβόμην", "ἐλαβόμεθα"], ["2nd", "ἐλάβου", "ἐλάβεσθε"], ["3rd", "ἐλάβετο", "ἐλάβοντο"]]
 aorist2_passive = [["1st", "ἐλήμφθην", "ἐλήμφθημεν"], ["2nd", "ἐλήμφθης", "ἐλήμφθητε"], ["3rd", "ἐλήμφθη", "ἐλήμφθησαν"]]
-show_three("2nd Aorist Indicative Active", aorist2_active, "2nd Aorist Indicative Middle", aorist2_middle, "2nd Aorist Indicative Passive", aorist2_passive)
+show_three("2nd Aor. Act. Ind.", aorist2_active, "2nd Aor. Mid. Ind.", aorist2_middle, "2nd Aor. Pass. Ind.", aorist2_passive)
 
 # Perfect Indicative
 perfect_active = [["1st", "λέλυκα", "λελύκαμεν"], ["2nd", "λέλυκας", "λελύκατε"], ["3rd", "λέλυκε(ν)", "λελύκασι(ν)"]]
 perfect_middle = [["1st", "λέλυμαι", "λελύμεθα"], ["2nd", "λέλυσαι", "λέλυσθε"], ["3rd", "λέλυται", "λέλυνται"]]
 perfect_passive = perfect_middle
-show_three("Perfect Indicative Active", perfect_active, "Perfect Indicative Middle", perfect_middle, "Perfect Indicative Passive", perfect_passive)
+show_three("Perf. Act. Ind.", perfect_active, "Perf. Mid. Ind.", perfect_middle, "Perf. Pass. Ind.", perfect_passive)
 
 # Pluperfect Indicative
 pluperfect_active = [["1st", "ἐλελύκη", "ἐλελύκεμεν"], ["2nd", "ἐλελύκης", "ἐλελύκετε"], ["3rd", "ἐλελύκει", "ἐλελύκεσαν"]]
 pluperfect_middle = [["1st", "ἐλελύμην", "ἐλελύμεθα"], ["2nd", "ἐλέλυσο", "ἐλέλυσθε"], ["3rd", "ἐλέλυτο", "ἐλέλυντο"]]
 pluperfect_passive = pluperfect_middle
-show_three("Pluperfect Indicative Active", pluperfect_active, "Pluperfect Indicative Middle", pluperfect_middle, "Pluperfect Indicative Passive", pluperfect_passive)
+show_three("Pluperf. Act. Ind.", pluperfect_active, "Pluperf. Mid. Ind.", pluperfect_middle, "Pluperf. Pass. Ind.", pluperfect_passive)
