b07161244cf74e55e8d72df8e28616422750e858
kohchingay <kohchingay@gmail.com>
2025-08-17 21:05:39 +0800

    Update indicative_mouseover.py

--- indicative_mouseover.py
+++ indicative_mouseover.py
@@ -14,6 +14,7 @@ import pandas as pd
 st.markdown("""
     <style>
         .block-container {
+            use_container_width="auto"
             max-width: unset;
             padding-left: 15rem;
             padding-right: 15rem;
